d5dccd724aecf54bb295af20e4a3df5ff8868a16
Rachelle Dauphin <rachdaup.tech@gmail.com>
2017-10-27 08:45:11 -0400

    Create quine.

---
+++ quine.rb
@@ -0,0 +1,182 @@
+source_code ={
+  code:
+%(source_code=;
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+get '/home' do
+  erb :home
+end
+
+get '/code' do
+  content_type :json
+  source_code.to_json
+end
+
+template :home do
+  <<-EOF
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>Quine</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css">
+  </head>
+
+  <body>
+  <div style="width:60%;margin:20px auto;">
+   <h1 style="text-align:center;">Source Code Request</h1>
+  <ol>
+    <li>Build a Sinatra app and deploy to Heroku.</li>
+    <li><div>
+    <p>The app should respond to the endpoints:</p>
+      <p>/home</p>
+      <p>/code</p>
+    </div></li>
+    <li><div>
+      <p>To test the /code endpoint the following command should run:</p>
+      <code>curl http://your-app.herokuapp.com/code</code>
+      <p>…and receive the JSON response:{ "code": "the source code of the app" }</p>
+    </div></li>
+    <li>For the /home endpoint, http://your-app.herokuapp.com/home will be visited in a web browser to see:</li>
+    <ul>
+      <li>A home page with introductions</li>
+      <li>A main section with a 3 second countdown</li>
+      <li>At the end of the countdown, an AJAX get request to the /code endpoint is made, and the code is then displayed in the main section properly formatted with syntax highlighting</li>
+    </ul>
+  </ol>
+  </div>
+  <div style="width:60%; margin:0 auto;" class="panel panel-warning">
+    <div class="panel-heading">Source Code Loading</div>
+    <div class="panel-body">
+    <h2 style="text-align:center;"></h2>
+    </div>
+  </div>
+
+  <script src="http://code.jquery.com/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+  <script>
+  hljs.initHighlightingOnLoad();
+  let counter = 3;
+  $(".panel-body h2").html(counter);
+  const interval = setInterval(function() {
+    counter--;
+    $(".panel-body h2").html(counter);
+    if (counter < 0) {
+        $(".panel-body").html(`<pre style="background:none;border:none;"><code id="test" class="hljs ruby"></code></pre>`);
+        requestCode();
+        clearInterval(interval);
+    }}, 1000);
+
+  function requestCode(){
+     $.ajax({
+          url: "/code",
+          success: (source) =>{
+          $(".panel").removeClass("panel-warning").addClass("panel-success");
+          $(".panel-heading").html("Source Code Loaded Successfully");
+          source = source.code.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+          $('#test').html(source);  
+          $('#test').each((index, block)=>{
+            hljs.highlightBlock(block);
+            });
+          }});
+  }
+  </script>
+  </body>
+</html>
+EOF
+end
+)
+}
+
+require 'sinatra'
+require 'sinatra/reloader'
+require 'json'
+
+get '/' do
+  redirect to('/home')
+end
+
+get '/home' do
+  erb :home
+end
+
+get '/code' do
+  content_type :json
+  source_code.to_json
+end
+
+template :home do
+  <<-EOF
+<html>
+  <head>
+    <meta http-equiv="content-type" content="text/html;charset=utf-8">
+    <title>Quine</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/railscasts.min.css">
+  </head>
+
+  <body>
+  <div style="width:60%;margin:20px auto;">
+   <h1 style="text-align:center;">Source Code Request</h1>
+  <ol>
+    <li>Build a Sinatra app and deploy to Heroku.</li>
+    <li><div>
+    <p>The app should respond to the endpoints:</p>
+      <p>/home</p>
+      <p>/code</p>
+    </div></li>
+    <li><div>
+      <p>To test the /code endpoint the following command should run:</p>
+      <code>curl http://your-app.herokuapp.com/code</code>
+      <p>…and receive the JSON response:{ "code": "the source code of the app" }</p>
+    </div></li>
+    <li>For the /home endpoint, http://your-app.herokuapp.com/home will be visited in a web browser to see:</li>
+    <ul>
+      <li>A home page with introductions</li>
+      <li>A main section with a 3 second countdown</li>
+      <li>At the end of the countdown, an AJAX get request to the /code endpoint is made, and the code is then displayed in the main section properly formatted with syntax highlighting</li>
+    </ul>
+  </ol>
+  </div>
+  <div style="width:80%; margin:0 auto;" class="panel panel-warning">
+    <div class="panel-heading">Source Code Loading</div>
+    <div class="panel-body">
+    <h2 style="text-align:center;"></h2>
+    </div>
+  </div>
+
+  <script src="http://code.jquery.com/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/highlight.min.js"></script>
+  <script>
+  hljs.initHighlightingOnLoad();
+  let counter = 3;
+  $(".panel-body h2").html(counter);
+  const interval = setInterval(function() {
+    counter--;
+    $(".panel-body h2").html(counter);
+    if (counter < 0) {
+        $(".panel-body").html(`<pre style="background:none;border:none;"><code id="test" class="hljs ruby"></code></pre>`);
+        requestCode();
+        clearInterval(interval);
+    }}, 1000);
+
+  function requestCode(){
+     $.ajax({
+          url: "/code",
+          success: (source) =>{
+          $(".panel").removeClass("panel-warning").addClass("panel-success");
+          $(".panel-heading").html("Source Code Loaded Successfully");
+          source = source.code.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+          $('#test').html(source);  
+          $('#test').each((index, block)=>{
+            hljs.highlightBlock(block);
+            });
+          }});
+  }
+  </script>
+  </body>
+</html>
+EOF
+end
